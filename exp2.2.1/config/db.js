cc2b37b03f8faa86b7686d9a5a87f7f14c3c6e2b
anv321 <sha.anve396@gmail.com>
2026-03-23 14:10:14 +0530

    Add files via upload

---
+++ exp2.2.1/config/db.js
@@ -0,0 +1,7 @@
+// config/db.js
+export const connectDB = async () => {
+  console.log('Using in-memory database (no MongoDB)');
+};
+
+// Mock connection for demo
+console.log('DB connected (in-memory)');
